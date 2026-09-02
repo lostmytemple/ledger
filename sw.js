9bd416fa0d7b80223491f23612f9615fbeaf3441
lostmytemple <74536540+lostmytemple@users.noreply.github.com>
2026-09-02 09:10:07 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const C='ledger-v9';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;})
+    .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
